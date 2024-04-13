0a7a28899f0f13824e614501d243cba8777ae6e8
Vishal <vishal2573003@gmail.com>
2024-04-13 23:16:57 +0530

    added component and button

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,17 @@
+export default function Card(){
+    return(
+        <div className="container mx-auto">
+      <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-4 py-10 lg:px-12 md:px-8 px-4">
+        
+        <div className="h-full">
+          <div className="self-center mx-auto rounded-2xl bg-white">
+            <div className="text-center text-xl font-bold py-2">Title</div>
+            <div><img className="mx-auto py-2" src="http://books.google.com/books/content?id=_eyJAAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" alt="" /></div>
+            <div className="text-center py-2 border-t-4  text-wrap px-4">skdjkjdglkjsgkljsskldjgks jdglkjaslkgjklsjgkljskgljsklgjskjgl sj ksldgjklgjkl asjglkjklgjl skjglsjglsjglksjkljgjslgjlkgjljgls jgj jk jgljalgg</div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+    );
+}
