cbb65b17b47e9df2123ce952dcc62a604cbabfbc
Vishal <vishal2573003@gmail.com>
2024-04-14 22:09:36 +0530

    added label

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -16,11 +16,15 @@ export default function SearchBar(){
 
     return(
         <div className="grid place-items-center h-dvh">
-            <div className="lg:w-2/5 md:w-2/4 w-3/4 h-10 rounded-2xl md:text-xl sm:text-lg text-sm overflow-hidden bg-white relative">
-                <form onSubmit={handleClick} className="w-full h-full">
-                    <input type="text" className="w-4/5 h-full p-5 focus:outline-none" required value={value} onChange={(e:any) => setValue(e.target.value)}/>
-                    <button type="submit" className="w-1/5 rounded-2xl bg-slate-300 h-full absolute text-white hover:bg-slate-400">Search</button>
-                </form>
+            <div className="grid w-full justify-items-center">
+                <div className="mb-10 text-2xl font-bold text-slate-600">Enter a book name!</div>
+                <div className="lg:w-2/5 md:w-2/4 w-3/4 h-10 rounded-2xl md:text-xl sm:text-lg text-sm overflow-hidden bg-white relative">
+                    <form onSubmit={handleClick} className="w-full h-full">
+                        <input type="text" className="w-4/5 h-full p-5 focus:outline-none" required value={value} onChange={(e:any) => setValue(e.target.value)}/>
+                        <button type="submit" className="w-1/5 rounded-2xl bg-slate-300 h-full absolute text-white hover:bg-slate-400">Search</button>
+                    </form>
+                </div>
+
             </div>
         </div>
     )
