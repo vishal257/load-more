816a2fe85c848698747af14ce6dc65c94913521d
Vishal <vishal2573003@gmail.com>
2024-04-14 22:02:05 +0530

    fixed description issue

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -21,7 +21,7 @@ export default async function getData(page:number, query:string) {
   for (const item of items) {
     let title = item.volumeInfo.title;
     let image = item.volumeInfo.imageLinks?.thumbnail || 'https://www.bookdeal.com/images/no-image.png';
-    let description = item.volumeInfo.description;
+    let description = item.volumeInfo.description || 'No Description Available!';
     let link = item.volumeInfo.infoLink;
 
     data.push({title:title, image:image, description:description, link:link});
