b682ab9201e0afe8450d52afd4ad6837752eda24
Vishal <vishal2573003@gmail.com>
2024-04-14 22:54:09 +0530

    improved overall UI

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
   return (
     <SuspenseHandler>
       <div className="w-full text-center py-5 mt-5 text-2xl font-bold text-slate-600 grid grid-cols-1">
-        <div className="ms-14 absolute md:block hidden cursor-pointer" onClick={handleGoback}>&lt;&lt; Go Back</div>
+        <div className="ms-14 absolute md:block hidden cursor-pointer" onClick={handleGoback}>&lt;&lt; <span className="underline">Go Back</span></div>
         <div className="justify-self-center">Showing Results For: {query}</div> 
         <div className="me-14 absolute md:block hidden justify-self-end">Results:{count+12}</div>
       </div>
