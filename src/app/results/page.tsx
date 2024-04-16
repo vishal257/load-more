f26432c0f2f04c46aa7171c1fb2413d4bb889263
Vishal <vishal2573003@gmail.com>
2024-04-16 15:47:41 +0530

    fixed useSearchParams

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -4,6 +4,7 @@ import { Suspense, useEffect, useRef, useState } from "react";
 import Card from "../components/Card";
 import getData from "../lib/data";
 import { useSearchParams, useRouter } from "next/navigation";
+import { renderToStaticMarkup } from "react-dom/server";
 
 
 interface RData{
@@ -19,7 +20,7 @@ function SuspenseHandler({children}:{children:React.ReactNode}){
   )
 }
 
-export default function Home() {
+function HomeHandler() {
 
   const [count, setCount] = useState(0);
   const [data, setData] = useState<RData[]>([]);
@@ -57,7 +58,7 @@ export default function Home() {
   }
 
   return (
-    <SuspenseHandler>
+    <>
       <div className="w-full text-center py-5 mt-5 text-2xl font-bold text-slate-600 grid grid-cols-1">
         <div className="ms-14 absolute md:block hidden cursor-pointer" onClick={handleGoback}>&lt;&lt; <span className="underline">Go Back</span></div>
         <div className="justify-self-center">Showing Results For: {query}</div> 
@@ -69,6 +70,14 @@ export default function Home() {
         <button className="rounded-full bg-black text-white py-4 px-10 hover:bg-slate-800" onClick={handleLoadMore}>Load More</button>
       </div>
     </div>
-    </SuspenseHandler>
+    </>
   );
+}
+
+export default function Home(){
+  return(
+    <SuspenseHandler>
+      <HomeHandler/>
+    </SuspenseHandler>
+  )
 }
